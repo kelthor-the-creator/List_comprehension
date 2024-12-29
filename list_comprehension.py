98f442fc82e54b35083819409cbf83718dc0c5cc
rojae <rojaerichards@live.co.uk>
2024-12-29 18:00:39 +0000

    Created file with functions for list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def listFrmRng():
+    a = [x for x in range(0,10)]
+    print(a)
+
+listFrmRng()
+
+def doubleInList():
+    b = [6.6,7,8,9,1,3]
+    c = [x**2 for x in b]
+    print(c)
+
+doubleInList()
